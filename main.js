aca2cc35e195a8b9420687c642d555f3597676d0
nicolashidalgo1989 <nicolas.hidalgo.1989@gmail.com>
2018-05-29 23:45:55 -0400

    js for svg

---
+++ main.js
@@ -0,0 +1,25 @@
+var svg = document.querySelector('#cable');
+//reference https://jsfiddle.net/83qpktrx/2/
+//dimension of svg
+var svgRect = svg.getBoundingClientRect();
+var svgw = svgRect.width;
+var svgh = svgRect.height;
+//center point of svg
+var xcenter = svgw/2;
+var ycenter = svgh/2;
+//http://stackoverflow.com/a/3642265/1869660
+function makeSVGElement(tag, attrs) {
+    var el= document.createElementNS('http://www.w3.org/2000/svg', tag);
+    for (var k in attrs) {
+        el.setAttribute(k, attrs[k]);
+    }
+    return el;
+}
+var cable = makeSVGElement('path', {    cx: xcenter,
+                                        cy: ycenter,
+                                        r: 10,
+                                        stroke: 'red',
+                                        'stroke-width': 2,
+                                        fill: 'orange' }
+);
+svg.appendChild(cable);
